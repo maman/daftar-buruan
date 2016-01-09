53ae3cbe7222156bb7f8ef7132c631334960701c
Prasetyo <jowy@oiry.net>
2016-01-09 16:56:10 +0700

    check env in view

--- app.go
+++ app.go
@@ -3,9 +3,17 @@ package main
 import (
 	_ "daftar-buruan/routes"
 
+	"os"
+
 	"github.com/astaxie/beego"
 )
 
 func main() {
+	beego.AddFuncMap("env", getEnv)
 	beego.Run()
+}
+
+func getEnv(in string) (out string) {
+	out = os.Getenv(in)
+	return
 }
